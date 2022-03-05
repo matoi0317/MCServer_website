b20990b84aacf344c9f30d6c3ecffbffce5b66a9
BuildTools <unconfigured@null.spigotmc.org>
2022-03-05 14:23:28 +0900

    edjo

--- components/SlideshowDiv.vue
+++ components/SlideshowDiv.vue
@@ -1,42 +1,79 @@
 <template>
 <div>
-    <client-only>
-        <swiper :options="swiperOption">
+    <div class="example-3d">
+        <swiper class="swiper" :options="swiperOption">
           <swiper-slide>Slide 1</swiper-slide>
           <swiper-slide>Slide 2</swiper-slide>
           <swiper-slide>Slide 3</swiper-slide>
+          <swiper-slide>Slide 4</swiper-slide>
+          <swiper-slide>Slide 5</swiper-slide>
+          <swiper-slide>Slide 6</swiper-slide>
+          <swiper-slide>Slide 7</swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
         </swiper>
-      </client-only>
-      <div slot="pagination" class="swiper-pagination" />
-      <div slot="button-prev" class="swiper-button-prev" />
-      <div slot="button-next" class="swiper-button-next" />
-    </div>
+      </div>
 </div>
 </template>
 <script>
-export default {
-  data() {
-    return {
-      swiperOption: {
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false
-        },
-        slidesPerView: 1,
-        loop: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev"
+  import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+  import 'swiper/css/swiper.css'
+  export default {
+    name: 'swiper-example-3d-coverflow',
+    title: '3D Coverflow effect',
+    components: {
+      Swiper,
+      SwiperSlide
+    },
+    data() {
+      return {
+        swiperOption: {
+          effect: 'coverflow',
+          grabCursor: true,
+          centeredSlides: true,
+          slidesPerView: 'auto',
+          coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1
+          },
+          pagination: {
+            el: '.swiper-pagination'
+          }
         }
       }
-    };
+    }
   }
-};
 </script>
-<style scoped>
-
-</style>
+<style lang="scss" scoped>
+.example-3d {
+      width: 100%;
+      height: 400px;
+      padding-top: 50px;
+      padding-bottom: 50px;
+    }
+    .swiper {
+      height: 100%;
+      width: 100%;
+      .swiper-slide {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 300px;
+        height: 300px;
+        text-align: center;
+        font-weight: bold;
+        font-size: 20;
+        background-color: #2C8DFB;
+        background-position: center;
+        background-size: cover;
+        color: #000;
+        border-radius: 10px;
+      }
+      .swiper-pagination {
+        /deep/ .swiper-pagination-bullet.swiper-pagination-bullet-active {
+          background-color: #fff;
+        }
+      }
+    }
+  </style>
