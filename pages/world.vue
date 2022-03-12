eb0d6bc7b46efa84f523dd4d1e7687111c475a49
matoi0317 <matoi.shibanuma@gmail.com>
2022-03-12 15:43:17 +0900

    world

--- pages/world.vue
+++ pages/world.vue
@@ -1,9 +1,132 @@
 <template>
-<div>ワールド紹介ページ</div>
+    <div class="test">
+        <div class="box">
+            <div class="content">
+                <img src="../components/athletic.png">
+            </div>
+            <div class="text">
+                テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+            </div>
+        </div>
+        <div class="box">
+            <div class="content">
+                <img src="../components/athletic.png">
+            </div>
+            <div class="text">
+                テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+            </div>
+        </div>
+        <div class="box">
+            <div class="content">
+                <img src="../components/athletic.png">
+            </div>
+            <div class="text">
+                テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+            </div>
+        </div>
+        <div class="box">
+            <div class="content">
+                <img src="../components/athletic.png">
+            </div>
+            <div class="text">
+                テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+            </div>
+        </div>
+        <div class="box">
+            <div class="content">
+                <img src="../components/athletic.png">
+            </div>
+            <div class="text">
+                テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+            </div>
+        </div>
+        <div class="box">
+            <div class="content">
+                <img src="../components/athletic.png">
+            </div>
+            <div class="text">
+                ここに説明
+            </div>
+        </div>
+        <div class="box">
+            <div class="content">
+                <img src="../components/athletic.png">
+            </div>
+            <div class="text">
+                テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+            </div>
+        </div>
+        <div class="box">
+            <div class="content">
+                <img src="../components/athletic.png">
+            </div>
+            <div class="text">
+                テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+            </div>
+        </div>
+        <div class="box">
+            <div class="content">
+                <img src="../components/athletic.png">
+            </div>
+            <div class="text">
+                テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+            </div>
+        </div>
+        <div class="box">
+            <div class="content">
+                <img src="../components/athletic.png">
+            </div>
+            <div class="text">
+                テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 </script>
 
 <style scoped>
+html{
+    background-color: #f5f5f5;
+}
+
+.test{
+    width:90%;
+    height: calc(100vh - 100px);
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+
+}
+
+.box{
+    width:25%;
+    height:50%;
+    background-color: black;
+    margin: 3%;
+    color:white;
+}
+
+.box .content{
+    width:90%;
+    background-color: red;
+    height:50%;
+    margin: 0 auto;
+    margin-top:5%;
+    margin-bottom: 5%;
+}
+
+img{
+    width:100%;
+    height:100%;
+}
+
+.box .text{
+    margin: 0 auto;
+    width:90%;
+    text-align: center;
+}
 </style>
