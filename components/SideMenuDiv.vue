93b1bae755548011defc8dcc541b705b1da4e34c
matoi0317 <matoi.shibanuma@gmail.com>
2022-03-19 15:03:57 +0900

    world

--- components/SideMenuDiv.vue
+++ components/SideMenuDiv.vue
@@ -1,10 +1,28 @@
 <template>
-<div>
+<div class="content">
+    <div class="name">・NULL Serverへようこそ</div>
+    <div class="adress">・IP:192.168.1.12</div>
+    <div class="ex">NULLサーバー(旧McKingDomサーバー)は中高生によって1から作られたMinecraftサーバーです。みんなが楽しく遊べるよう日々賢明に開発し、常に進化を遂げています。ぜひ皆さんも楽しいNULL Lifeをお楽しみください。</div>
 </div>
 </template>
 <script>
 
 </script>
 <style scoped>
+.content{
+    width:100%;
+    height: 50%;
+    position: absolute;
+    bottom: 0;
+    margin: 0;
+    font-size: 15px;
+    inline-size:29%;
+    line-height: 2;
+    padding: 16px 32px;
+}
+
+.ex{
+    padding-top: 30px;
+}
 
 </style>
