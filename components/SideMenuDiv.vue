0f4908104a94ca4232aecdb052c35484834c81da
matoi0317 <matoi.shibanuma@gmail.com>
2022-03-26 14:03:59 +0900

    aaa

--- components/SideMenuDiv.vue
+++ components/SideMenuDiv.vue
@@ -9,6 +9,7 @@
 
 </script>
 <style scoped>
+
 .content{
     width:100%;
     height: 50%;
---
+++ pages/rule.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>ルール
+    </div>
+
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
